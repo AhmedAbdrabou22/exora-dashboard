a431deef22f66d83cf358aa453a669fe169c98b2
aabdrabou <aabdrabou819@gmail.com>
2026-01-23 20:58:57 +0200

    handle all adds prducts and colors and contacts

--- src/components/templates/products/MainData.tsx
+++ src/components/templates/products/MainData.tsx
@@ -88,7 +88,7 @@ function MainData({ update }: { update?: Product | null }) {
           <div className="flex flex-col gap-1 text-right">
             <label className="text-sm font-medium">نوع الخصم</label> 
             <select
-              name="discount_type"
+              name="discount_type"  
               value={values.discount_type}
               onChange={(e) => {
                 const event = e as any;
